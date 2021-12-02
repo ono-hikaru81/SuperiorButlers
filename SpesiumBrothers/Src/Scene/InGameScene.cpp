3f6c1652c6eb9eb5984ea9efa56a92985e35e690
ono-hikaru81 <Onohikaru0801@gmail.com>
2021-12-03 01:24:18 +0900

    デバッグ用追加(後程削除)
    ・デバッグ用にInGameSceneにモンスターとカメラオブジェクトを追加

--- SpesiumBrothers/Src/Scene/InGameScene.cpp
+++ SpesiumBrothers/Src/Scene/InGameScene.cpp
@@ -7,6 +7,8 @@
 namespace spesium {
     namespace scene {
         InGameScene::InGameScene() {
+            monster = new character::Monster();
+            camera = new tool::Camera();
         }
 
         InGameScene::~InGameScene() {
@@ -16,10 +18,16 @@ namespace spesium {
             if ( inputManager.lock()->IsKeyPushed( KEY_INPUT_RETURN ) ) {
                 SceneManager::Instance().lock()->NextScene = Scene::Result;
             }
+
+            static Vector3 origin { 0.0, 0.0, 0.0 };
+
+            monster->Exec();
+            camera->UpdatePos( monster->Pos, origin );
         }
 
         void InGameScene::Draw() {
             DrawString( 120, 120, "InGame", GetColor( 0, 0, 0 ) );
+            monster->Draw();
         }
     }  // namespace scene
 }  // namespace spesium
