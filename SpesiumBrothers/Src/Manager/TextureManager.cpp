457b2eced92cd88b3941e0cf1283de42bdf4c0ec
ono-hikaru81 <66341091+ono-hikaru81@users.noreply.github.com>
2021-12-02 16:19:13 +0900

    TextureManger修正 (#21)

--- SpesiumBrothers/Src/Manager/TextureManager.cpp
+++ SpesiumBrothers/Src/Manager/TextureManager.cpp
@@ -1,29 +1,21 @@
+
 #include "TextureManager.h"
 
-namespace spesium
-{
-	TextureManager::TextureManager()
-	{
-	}
+namespace spesium {
+    void TextureManager::LoadTexture( const std::string& name, const std::string& filePath ) {
+        auto handle { LoadGraph( filePath.c_str() ) };
+        if ( handle == -1 ) { return; }
 
-	TextureManager::~TextureManager()
-	{
-	}
+        TextureList.emplace( name, handle );
+    }
 
-	void TextureManager::LoadTexture(const std::string& name, const std::string& filePath)
-	{
-		TextureList.emplace(name, LoadGraph(filePath.c_str()));
-	}
+    void TextureManager::ReleaseTexture() {
+        TextureList.clear();
+        InitGraph();
+    }
 
-	void TextureManager::ReleaseTexture()
-	{
-		TextureList.clear();
-		InitGraph();
-	}
-
-	std::optional<int32_t> TextureManager::GetTexture(const std::string& name)
-	{
-		const auto& tmp = TextureList.find(name);
-		return(tmp == TextureList.end()) ? std::nullopt : std::make_optional( tmp->second);
-	}
-}
+    std::optional<int32_t> TextureManager::GetTexture( const std::string& name ) {
+        auto tmp { TextureList.find( name ) };
+        return ( tmp == TextureList.end() ) ? std::nullopt : std::make_optional( tmp->second );
+    }
+}  // namespace spesium
