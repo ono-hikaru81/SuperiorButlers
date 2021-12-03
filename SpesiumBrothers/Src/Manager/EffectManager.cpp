28207fd1e3465ade986ff4cde92bf89bc5dc12ed
Takuto-Miyoshi <miyoshi.taku10@gmail.com>
2021-12-03 10:58:23 +0900

    作り中
    画像読み込み部分だけ

--- SpesiumBrothers/Src/Manager/EffectManager.cpp
+++ SpesiumBrothers/Src/Manager/EffectManager.cpp
@@ -1,6 +1,28 @@
 ﻿
+#include <DxLib.h>
+
 #include <Manager/EffectManager.hpp>
 
 namespace spesium {
+    void EffectManager::Load( const std::string& name_, const std::string& filePath_ ) & {
+        auto handle { LoadGraph( filePath_.c_str() ) };
+        if ( handle == -1 ) { return; }
+        list.try_emplace( name_, std::move( handle ) );
+    }
 
-}
+    void EffectManager::Clear() & noexcept {
+        for ( auto&& data : std::as_const( list ) ) {
+            DeleteGraph( data.second );
+        }
+
+        list.clear();
+    }
+
+    auto EffectManager::Get( const std::string& name_ ) const& noexcept -> std::optional<int32_t> {
+        auto res { list.find( name_ ) };
+        return ( res == list.end() ) ? std::nullopt : std::make_optional( res->second );
+    }
+
+    void EffectManager::Generate( const std::string& name_, const Vector3<double>& position_ ) const& noexcept {
+    }
+}  // namespace spesium
--- SpesiumBrothers/Src/Manager/EffectManager.hpp
+++ SpesiumBrothers/Src/Manager/EffectManager.hpp
@@ -6,12 +6,29 @@
 
 #include <Utility/Singleton.h>
 
+#include <Utility/Vector.hpp>
+#include <map>
+#include <optional>
+#include <string>
+
 namespace spesium {
     class EffectManager : public Singleton<EffectManager> {
        public:
         EffectManager() = default;
 
         ~EffectManager() = default;
+
+       public:
+        void Load( const std::string& name_, const std::string& filePath_ ) &;
+
+        void Clear() & noexcept;
+
+        [[nodiscard]] auto Get( const std::string& name_ ) const& noexcept -> std::optional<int32_t>;
+
+        void Generate( const std::string& name_, const Vector3<double>& position_ ) const& noexcept;
+
+       private:
+        std::map<std::string, int32_t> list {};
     };
 }  // namespace spesium
 
