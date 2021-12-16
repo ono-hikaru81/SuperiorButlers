c7f2c97def0aa45a05a69b17ff9446261dae83cc
ono-hikaru81 <66341091+ono-hikaru81@users.noreply.github.com>
2021-12-16 14:02:53 +0900

    人間キャラのクラス追加 (#33)
    ・Human.h、.Human.cppを追加
    ・クラス作成
    ・メソッド追加

---
+++ SpesiumBrothers/Src/Character/Human.cpp
@@ -0,0 +1,105 @@
+﻿
+#include <Character/Human.h>
+#include <DxLib.h>
+
+namespace spesium {
+    namespace character {
+        Human::Human() {
+            status = {
+                { Vector3( 0.0, 0.0, 0.0 ) },  // 座標
+                0.0,  // 回転角
+
+                0,  // 体力
+                0,  // シールド量
+                0,  // 攻撃力
+                1.0,  // 速度
+                10.0,  // 最大速度
+                10.0,  // ジャンプ力
+            };
+
+            motionModel = {};
+
+            motionModelName = {};
+
+            playMotionTime = {};
+
+            playTotalMotionTime = {};
+        }
+
+        Human::~Human() {
+        }
+
+        void Human::Draw() {
+            MV1SetPosition( monsterModel, status.pos );
+
+            MV1SetRotationXYZ( monsterModel, Vector3( 0.0, status.angle / 180.0 * DX_PI, 0.0 ) );
+
+            MV1DrawModel( monsterModel );
+
+#if _DEBUG
+            DrawFormatString( 0, 0, GetColor( 0, 255, 0 ), "モンスター座標[%d,%d]", static_cast<int32_t>( status.pos.X ), static_cast<int32_t>( status.pos.Y ) );
+#endif  // _DEBUG
+        }
+
+        void Human::WaitMotion() {
+            // 再生時間を0に戻す
+            if ( playMotionTime.wait >= playTotalMotionTime.wait ) {
+                playMotionTime.wait = 0.0f;
+            }
+
+            MV1SetAttachAnimTime( monsterModel, motionModel.wait, playMotionTime.wait );
+        }
+
+        void Human::DashMotion() {
+            MV1SetAttachAnimTime( monsterModel, motionModel.dash, playMotionTime.dash );
+        }
+
+        void Human::JumpMotion() {
+            MV1SetAttachAnimTime( monsterModel, motionModel.jump, playMotionTime.jump );
+        }
+
+        void Human::TwoStepJumpMotion() {
+            MV1SetAttachAnimTime( monsterModel, motionModel.twoStepJump, playMotionTime.twoStepJump );
+        }
+
+        void Human::GuardMotion() {
+            MV1SetAttachAnimTime( monsterModel, motionModel.guard, playMotionTime.guard );
+        }
+
+        void Human::NeutralAttackMotion() {
+            MV1SetAttachAnimTime( monsterModel, motionModel.neutralAttack, playMotionTime.neutralAttack );
+        }
+
+        void Human::StrongAttackMotion() {
+            MV1SetAttachAnimTime( monsterModel, motionModel.strongAttack, playMotionTime.strongAttack );
+        }
+
+        void Human::AerialNeutralAttackMotion() {
+            MV1SetAttachAnimTime( monsterModel, motionModel.aerialNeutralAttack, playMotionTime.aerialNeutralAttack );
+        }
+
+        void Human::AerialStrongAttackMotion() {
+            MV1SetAttachAnimTime( monsterModel, motionModel.aerialStrongAttack, playMotionTime.aerialStrongAttack );
+        }
+
+        void Human::LandingMotion() {
+            MV1SetAttachAnimTime( monsterModel, motionModel.landing, playMotionTime.landing );
+        }
+
+        void Human::SmallHitBackMotion() {
+            MV1SetAttachAnimTime( monsterModel, motionModel.smallHitBackMotion, playMotionTime.landing );
+        }
+
+        void Human::BigHitBackMotion() {
+            MV1SetAttachAnimTime( monsterModel, motionModel.bigHitBackMotion, playMotionTime.bigHitBackMotion );
+        }
+
+        void Human::FallMotion() {
+            MV1SetAttachAnimTime( monsterModel, motionModel.fall, playMotionTime.fall );
+        }
+
+        void Human::TurnMotion() {
+            MV1SetAttachAnimTime( monsterModel, motionModel.turn, playMotionTime.turn );
+        }
+    }  // namespace character
+}  // namespace spesium
---
+++ SpesiumBrothers/Src/Character/Human.h
@@ -0,0 +1,68 @@
+﻿
+#ifndef HUMAN_H
+#define HUMAN_H
+
+#include <Character/CharacterBase.h>
+
+namespace spesium {
+    namespace character {
+        class Human : public CharacterBase {
+           public:
+            Human();
+            ~Human() override;
+
+           public:
+            void Draw() override;
+
+           private:
+            /// @breif 待機モーション
+            void WaitMotion() override;
+
+            /// @breif ダッシュモーション
+            void DashMotion() override;
+
+            /// @breif ジャンプモーション
+            void JumpMotion() override;
+
+            /// @breif 2段ジャンプモーション
+            void TwoStepJumpMotion() override;
+
+            /// @breif ガードモーション
+            void GuardMotion() override;
+
+            /// @breif 弱攻撃モーション
+            void NeutralAttackMotion() override;
+
+            /// @breif 強攻撃モーション
+            void StrongAttackMotion() override;
+
+            /// @breif 空中弱攻撃モーション
+            void AerialNeutralAttackMotion() override;
+
+            /// @breif 空中強攻撃モーション
+            void AerialStrongAttackMotion() override;
+
+            /// @breif 着地モーション
+            void LandingMotion() override;
+
+            /// @breif ヒットバックモーション小
+            void SmallHitBackMotion() override;
+
+            /// @breif ヒットバックモーション大
+            void BigHitBackMotion() override;
+
+            /// @breif 落下モーション
+            void FallMotion() override;
+
+            /// @brief 振り向きモーション
+            void TurnMotion() override;
+
+           public:
+            /// @brief アクセサ
+            /// @param (status.pos) 座標
+            BasicProperty<Vector3<double>> Pos { status.pos };
+        };
+    }  // namespace character
+}  // namespace spesium
+
+#endif
